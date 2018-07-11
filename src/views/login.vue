eedd44b75ec77c0ae620c6a07aeb790294037387
zff <zhengfangfang@chinadas.com>
2018-07-11 18:31:51 +0800

    注册页面重构

--- src/views/login.vue
+++ src/views/login.vue
@@ -10,23 +10,6 @@
                 <div class="login-tab">
                   <div id="role" v-for="(item,index) in tabs" :class="{active:nowIndex == index}" @click="changeIndex(index)">{{item.title}}</div>
                 </div>
-                <!--<form id="form" class="clearfix" method="post">-->
-                    <!--<div>-->
-                      <!--<div class="flex-one"><label for="">账号：</label></div>-->
-                      <!--<div class="flex-manager"><input type="text" class="login-input input"></div>-->
-                    <!--</div>-->
-                    <!--<div>-->
-                      <!--<div class="flex-one"><label for="">密码：</label></div>-->
-                      <!--<div class="flex-manager"><input type="text" class="login-input input"></div>-->
-                    <!--</div>-->
-                    <!--<span class="forget pointer fr" v-on:click="forgetPass">忘记密码?</span>-->
-                    <!--<button class="form-btn" v-on:click="sendLogin($event,isAdmin)">登陆</button>-->
-                    <!--<span class="reg fr">没有账号-->
-                        <!--<router-link to="/register" target="_blank">-->
-                          <!--<span>立即注册</span>-->
-                        <!--</router-link>-->
-                    <!--</span>-->
-                <!--</form>-->
                 <!--普通用户模块-->
                 <div class="login-con" v-if="nowIndex == 0">
                   <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="login-ruleForm" :label-position="labelPosition" label-width="65px">
@@ -181,12 +164,6 @@ export default {
     .left-tip {
       margin-top:125px;
       margin-left:80px;
-      font-size: 30px;
-      color: #d0021b;
-      div {
-        width: 20px;
-        padding: 10px;
-      }
     }
     // 右侧 form
     .right-login {
@@ -212,7 +189,7 @@ export default {
         margin-left:65px;
         #role {
           flex:1;
-          font-size: 16px;
+          font-size: 14px;
           color: #f7aa32;
           cursor: pointer;
           border: 1px solid #f7aa32;
@@ -242,6 +219,7 @@ export default {
         border:1px solid #f7aa32;
         color:#fff;
         font-weight:bold;
+        width:100%;
         &:hover{
           background:#D89020;
           border-color: #D89020;
@@ -255,59 +233,6 @@ export default {
         em{
           cursor: pointer;
           color: #f7aa32;
-        }
-      }
-      #form {
-        margin-top: 25px;
-        line-height: 50px;
-        .forget {
-          font-size: 14px;
-          color: red;
-          line-height: 30px !important;
-        }
-        .form-btn {
-          width: 100%;
-          height: 40px;
-          border: 0;
-          cursor: pointer;
-          background-color: #d89020;
-          font-size: 20px;
-          color: #ffffff;
-          letter-spacing: 1px;
-        }
-        .reg {
-          font-size: 16px;
-          color: #ffffff;
-          span{
-            font-size: 16px;
-            color: #b5071b;
-            line-height: 21px;
-            cursor: pointer;
-          }
-        }
-      }
-      #form > div {
-        display: flex;
-        justify-content: space-around;
-        .login-input {
-          width: 340px;
-          height: 40px;
-          background: transparent;
-          border: 1px solid #ffffff;
-          padding-left: 10px;
-          font-size: 20px;
-          color: #fff;
-        }
-        .flex-one {
-          flex: 1;
-        }
-        .flex-manager {
-          flex: 3;
-        }
-        label {
-          font-size: 20px;
-          color: #ffffff;
-          letter-spacing: 2px;
         }
       }
     }
--- src/views/register.vue
+++ src/views/register.vue
@@ -4,35 +4,24 @@
           <div class="top-panel"></div>
           <div :style="bgdiv" class="center-panel clearfix">
               <div class="left-tip fl">
-                <div>深入经藏</div><div>智慧如海</div>
+                <p><img src="../assets/tit.png" alt=""></p>
               </div>
               <div class="right-register fr">
-                <!-- <div><span class="register-title">藏经图文标注平台</span></div> -->
-                <el-form  ref="form" :model="form" id="form" rules="ruleForm" class="form-bg clearfix" method="post">
-                  <div class="reg-title">
-                    注册
-                  </div>
-                  <div class="form-item">
-                    <div>
-                      <p>邮箱</p>
-                      <el-input class="reg-input" v-model="form.email" type="text" clearable/>
-                    </div>
-                  </div>
-                  <div class="form-item">
-                    <div>
-                      <p>密码</p>
-                      <el-input class="reg-input" type="text" placeholder="包含数字和字母6-18位" v-model="form.pass" clearable/>
-                    </div>
-                  </div>
-                  <div class="form-item">
-                    <div>
-                      <p>确认密码</p>
-                      <el-input class="reg-input" type="text" v-model="form.checkPass" clearable/>
-                    </div>
-                  </div>
-                  <div class="form-item">
-                    <el-button class="reg-btn input" id="register" @click="onSubmit">注册成功并登陆</el-button>
-                  </div>
+                <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="login-ruleForm" :label-position="labelPosition" label-width="65px">
+                  <h3 class="reg-title">注册</h3>
+                  <el-form-item class="item" prop="email" label="邮箱：">
+                    <el-input v-model.trim="ruleForm.email" placeholder="请输入邮箱" auto-complete="off" class="need"></el-input>
+                  </el-form-item>
+                  <el-form-item class="item" prop="pass" label="密码：">
+                    <el-input type="password" v-model.trim="ruleForm.pass" placeholder="请输入密码" auto-complete="off" class="need"></el-input>
+                  </el-form-item>
+                  <el-form-item class="item" prop="pass" label="确认：">
+                    <el-input type="password" v-model.trim="ruleForm.pass" placeholder="请确认密码" auto-complete="off" class="need"></el-input>
+                  </el-form-item>
+                  <p class="log">已有账号，<em @click="goLogin">立即登录</em></p>
+                  <el-form-item class="sub-btn">
+                    <el-button type="big" class="reg-btn" @click="">立即注册</el-button>
+                  </el-form-item>
               </el-form>
               </div>
           </div>
@@ -43,34 +32,89 @@
 <script>
 export default {
   data() {
-    var validateNewPass = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入新密码"));
+    //邮箱前台验证
+    var validateEmail = (rule,value,cb) => {
+      var pattern = /^([a-zA-Z0-9]+[-|_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[-|_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+      if(value === ''){
+        cb(new Error('请输入邮箱'))
+      }else if(!pattern.test(value)){
+        cb(new Error('请输入正确的邮箱'))
+      }else{
+        cb();
+      }
+    };
+    //新密码前台验证
+    var validatePass = (rule, value, cb) => {
+      var pattern = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/;
+      if (value === '') {
+        cb(new Error('请输入新密码'));
+      }else if(!pattern.test(value)){
+        cb(new Error('请输入6－20位字母数字混合密码'));
+      }else {
+        if (this.ruleForm.checkpass !== '') {
+          this.$refs.ruleForm.validateField('checkpass');
+        }
+        cb();
+      }
+    };
+    //确认新密码
+    var validateCheckpass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入新密码'));
+      } else if (value !== this.ruleForm.newpass) {
+        callback(new Error('两次输入密码不一致!'));
       } else {
-        // if (this.form.newpass !== "") {
-        // this.$refs.ruleForm.validateField("checkPass");
-        // }
         callback();
       }
     };
     return {
-      form: {
-        email: '',
-        pass: '',
-        checkPass: ''
-      },
       // 背景图
       bgdiv: {
            backgroundImage: 'url(' + require('assets/login-bg.jpg') + ')'
-          // backgroundColor: rgba(0,0,0,0.43)
-       },
-      msg: ""
+      },
+      labelPosition: 'right',
+      rules:{
+        email: [
+          {required: true,validator: validateEmail, trigger: 'blur'},
+          { min: 1, max: 42, message: '请输入正确的邮箱', trigger: 'blur' }
+        ],
+        pass: [
+          {required: true, validator: validatePass, trigger: 'blur'},
+        ],
+        checkpass: [
+          {required: true, validator: validateCheckpass, trigger: 'blur'},
+        ],
+      },
+      ruleForm:{
+        email:'',
+        pass:'',
+        checkpass:''
+      }
     };
   },
   methods: {
-    // 注册
-    onSubmit: () => {
-      alert(1);
+    //跳转到登录
+    goLogin(){
+      this.$router.push('/');
+    },
+    //注册
+    handleSubmit(formname){
+      this.$refs[formname].validate((valid) => {
+        if(valid){
+          this.logining = true;
+          let regParam = {
+            email:this.ruleForm.email,
+            password:this.ruleForm.pass
+          }
+          this.axios.post('',regParam).then((res) => {
+            this.logining = false;
+            this.$message({
+              message: '注册成功',
+              type: 'success'
+            })
+          })
+        }
+      })
     }
   }
 };
@@ -78,16 +122,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-.active {
-  background-color: #d89020;
-  border: 0 !important;
-}
-.tran {
-  background-color: transparent;
-}
-.pointer {
-  cursor: pointer;
-}
 .register {
   overflow: hidden;
 }
@@ -106,78 +140,44 @@ export default {
     background-size: cover;
     border-radius: 0 0 30px 30px;
     .left-tip {
-      padding: 90px 30px;
-      width: 20px;
-      line-height: 50px;
-      font-size: 30px;
-      color: #d0021b;
-      div {
-        width: 20px;
-        padding: 10px;
-      }
+      margin-top:125px;
+      margin-left:80px;
     }
     .right-register {
-      padding: 60px 80px;
-      // .register-title {
-      //   font-family: MicrosoftYaHei-Bold;
-      //   font-size: 48px;
-      //   color: #429bf0;
-      //   letter-spacing: 4.8px;
-      // }
-      .form-bg {
-        background: rgba(255,255,255,0.66);
+      margin-top:120px;
+      margin-right:80px;
+      padding:30px 20px;
+      width:300px;
+      background: rgba(255,255,255,0.4);
+      border-radius:3px;
+      .reg-title{
+        font-size:20px;
+        text-align: center;
+        color:#f7aa32;
+        margin-bottom:20px;
+        font-weight:bold;
+        letter-spacing: 3px;
       }
-      #form {
-        margin-top: 25px;
-        padding: 10px 20px; 
-        .reg-title {
-          font-size: 30px;
-          color: #429bf0;
-          font-weight: bold;
-          letter-spacing: 3px;
-          text-align: center;
+      .log{
+        text-align: right;
+        font-size:12px;
+        color: rgba(255,255,255,0.8);
+        margin-bottom:20px;
+        em{
+          cursor: pointer;
+          color: #f7aa32;
         }
-        .form-item {
-          padding: 20px 0;
-          // #register {
-          //   width: 100%;
-          //   height: 35px;
-          //   background: #d89020;
-          //   border: 0;
-          //   font-size: 20px;
-          //   color: #ffffff;
-          //   letter-spacing: 2px;
-          // }
+      }
+      .reg-btn{
+        background:#f7aa32;
+        border:1px solid #f7aa32;
+        color:#fff;
+        font-weight:bold;
+        width:100%;
+        &:hover{
+          background:#D89020;
+          border-color: #D89020;
         }
-        .form-item > div > p {
-          font-size: 20px;
-          color: #429bf0;
-          font-weight: bold;
-          letter-spacing: 2.5px;
-        }
-        .reg-input {
-          width: 290px;
-          margin-top: 10px;
-        }
-        .reg-btn {
-          width: 100%;
-          height: 50px;
-          background-color: #d89020;
-          font-size: 20px;
-          color: #ffffff;
-          letter-spacing: 2px;
-          border-color: #d89020;
-        }
-        // el-input {
-        //   width: 500px;
-        // }
-        // .form-item > div > el-input {
-        //   border: 1px solid #979797;
-        //   width: 290px;
-        //   height: 30px;
-        //   font-size: 18px;
-        //   padding-left: 10px;
-        // }
       }
     }
   }
